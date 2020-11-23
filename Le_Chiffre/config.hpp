a1fc789f9c6b5362cf27f98965ed6e2ec0f777a2
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2020-11-24 01:41:11 +0200

    Removed useless game state enum

--- Le_Chiffre/config.hpp
+++ Le_Chiffre/config.hpp
@@ -6,9 +6,5 @@ namespace {
 	const wchar_t* TARGET = L"csgo.exe";
 	const wchar_t* CLIENT_DLL = L"client.dll";
 	const wchar_t* ENGINE_DLL = L"engine.dll";
-
-	enum client_states {
-		LOBBY, LOADING, CONNECTING, CONNECTED = 5, INGAME
-	};
 }
 #endif
