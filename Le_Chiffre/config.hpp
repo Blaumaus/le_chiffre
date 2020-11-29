edd7a372844f4e40ea17ecbafda1c3e996d0e8a8
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2020-11-30 00:31:26 +0200

    Added HWND CS:GO window name

--- Le_Chiffre/config.hpp
+++ Le_Chiffre/config.hpp
@@ -6,6 +6,7 @@ namespace {
 	const wchar_t* TARGET = L"csgo.exe";
 	const wchar_t* CLIENT_DLL = L"client.dll";
 	const wchar_t* ENGINE_DLL = L"engine.dll";
+	LPCSTR WINDOW_NAME = "Counter-Strike: Global Offensive";
 
 	struct coords_vector {
 		float x, y, z;
