d7dde5a5b1f6bd83272dc135e2f4e001bd5ddf69
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2021-01-23 17:49:11 +0200

    Added methods to check if the cheat is updated to latest version

---
+++ Le_Chiffre/misc/utils.hpp
@@ -0,0 +1,46 @@
+#ifndef UTILS_HPP
+#define UTILS_HPP
+#pragma once
+#include "config.hpp"
+#include "web_utils.hpp"
+#include <iostream>
+
+// Manually gets the value of a key from a JSON string 
+// Sorry about that, but I just don't want to import a JSON parsing library only because of one simple task
+// I wrote all this code at 4 AM, so it probably needs some refactoring
+std::string json_parse_string(std::string data, std::string key) {
+	std::string buffer;
+	bool quote_opened = false;
+	bool key_found = false;
+
+	for (char const& c : data) {
+		if (c == '"') {
+			if (!quote_opened) quote_opened = true;
+			else {
+				if (key_found) return buffer;
+				if (buffer == key) key_found = true;
+				quote_opened = false;
+			}
+
+			buffer.clear();
+		}
+		else if (quote_opened) buffer.push_back(c);
+	}
+
+	return {};
+}
+
+std::string get_latest_version() {
+	std::string json = web::get(HOST, PATH);
+
+	return json != "ERROR" ? json_parse_string(json, "name") : "ERROR";
+}
+
+// {ERROR, LATEST}
+std::pair<bool, bool> is_latest() {
+	std::string ver = get_latest_version();
+
+	if (ver == "ERROR") return std::make_pair(true, false);
+	return std::make_pair(false, ver == CHEAT_VERSION);
+}
+#endif // !UTILS_HPP
