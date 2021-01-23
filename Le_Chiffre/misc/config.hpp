9e5f45a262a74502752367e90cd7dc9a353059b9
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2021-01-23 17:47:13 +0200

    Added cheat version and remote server vars & moved it into misc/ dir

---
+++ Le_Chiffre/misc/config.hpp
@@ -0,0 +1,19 @@
+#ifndef CONFIG_HPP
+#define CONFIG_HPP
+#pragma once
+#include <string>
+
+namespace {
+	const ::std::string CHEAT_VERSION = "v1.0.4";
+	const wchar_t* HOST = L"api.github.com";
+	const wchar_t* PATH = L"/repos/blaumaus/le_chiffre/tags?per_page=1";
+	const wchar_t* TARGET = L"csgo.exe";
+	const wchar_t* CLIENT_DLL = L"client.dll";
+	const wchar_t* ENGINE_DLL = L"engine.dll";
+	LPCSTR WINDOW_NAME = "Counter-Strike: Global Offensive";
+
+	struct coords_vector {
+		float x, y, z;
+	};
+}
+#endif
