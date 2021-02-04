057b93f3f2d70d8af15718353d1952ab0676c464
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2021-02-04 03:11:04 +0200

    added a donation request functionality

--- frontend/script.js
+++ frontend/script.js
@@ -5,6 +5,25 @@ const format_date = date => {
   return `${date[2]} ${months[date[1] - 1]} ${date[0]}`
 }
 
+const set_cookie = (key, val, expires = 1209600) => { // 2 weeks is the default expire time
+  document.cookie = `${key}=${val};Max-Age=${expires};path=/;samesite=strict`
+}
+
+const check_cookie = key => {
+  return document.cookie.split(';').some(item => item.trim().startsWith(`${key}=`))
+}
+
+const ask_donation = () => {
+  alert('Hello.\nIf you find this project useful and want me to update it regularly, consider donating a small amount to help me cover the costs of development.\n\nYour donations are appreciated!')
+}
+
+const check_donation = () => {
+  if (!check_cookie('donation')) {
+    set_cookie('donation', 1)
+    ask_donation()
+  }
+}
+
 document.addEventListener('DOMContentLoaded', () => {
   const desc = document.querySelector('#description > p')
   const download = document.getElementById('download_btn')
@@ -21,6 +40,8 @@ document.addEventListener('DOMContentLoaded', () => {
       download.href = download_url
     })
     .catch(e => console.log(e))
+
+  setTimeout(check_donation, 5000)
 
   insights.init('QZXDaLRWC1XvtWGM', { ignoreErrors: true })
   insights.trackPages({
