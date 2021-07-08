ef348cd20f965c5f1ea5365c9d91b47eec016423
Blue Mouse <durexp@protonmail.ch>
2021-07-08 19:15:17 +0300

    bumped version to 1.4.0

--- Le_Chiffre/misc/config.hpp
+++ Le_Chiffre/misc/config.hpp
@@ -6,8 +6,8 @@
 
 namespace {
 	// Config
-	const ::std::string CHEAT_VERSION = XorStr("v1.3.9");
-	const ::std::string RELEASE_DATE = XorStr("[19 June, 2021]");
+	const ::std::string CHEAT_VERSION = XorStr("v1.4.0");
+	const ::std::string RELEASE_DATE = XorStr("[08 July, 2021]");
 	const wchar_t* HOST = L"api.github.com";
 	const wchar_t* PATH = L"/repos/blaumaus/le_chiffre/tags?per_page=1";
 	const wchar_t* TARGET = L"csgo.exe";
