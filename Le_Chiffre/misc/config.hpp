1e65e2c32a0b9d2c4fc8797dccf1733956b5f719
Blue Mouse <durexp@protonmail.ch>
2021-11-10 23:17:58 +0200

    updated signatures

--- Le_Chiffre/misc/config.hpp
+++ Le_Chiffre/misc/config.hpp
@@ -6,8 +6,8 @@
 
 namespace {
 	// Config
-	const ::std::string CHEAT_VERSION = XorStr("v1.4.10");
-	const ::std::string RELEASE_DATE = XorStr("[05 November, 2021]");
+	const ::std::string CHEAT_VERSION = XorStr("v1.4.11");
+	const ::std::string RELEASE_DATE = XorStr("[10 November, 2021]");
 	const wchar_t* HOST = L"api.github.com";
 	const wchar_t* PATH = L"/repos/blaumaus/le_chiffre/tags?per_page=1";
 	const wchar_t* TARGET = L"csgo.exe";
