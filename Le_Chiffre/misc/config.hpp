607878a144064794e8867de2b0566593ae666339
Blue Mouse <35810911+Blaumaus@users.noreply.github.com>
2022-05-17 22:31:29 +0300

    updated signatures

--- Le_Chiffre/misc/config.hpp
+++ Le_Chiffre/misc/config.hpp
@@ -6,8 +6,8 @@
 
 namespace {
 	// Config
-	const ::std::string CHEAT_VERSION = XorStr("v1.4.24");
-	const ::std::string RELEASE_DATE = XorStr("[14 May, 2022]");
+	const ::std::string CHEAT_VERSION = XorStr("v1.4.25");
+	const ::std::string RELEASE_DATE = XorStr("[18 May, 2022]");
 	const wchar_t* HOST = L"api.github.com";
 	const wchar_t* PATH = L"/repos/blaumaus/le_chiffre/tags?per_page=1";
 	const wchar_t* TARGET = L"csgo.exe";
