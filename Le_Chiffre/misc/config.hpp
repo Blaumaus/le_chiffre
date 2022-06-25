86f1e5216bcc1275a4bf8f3048b8c0b772f0fdd0
Liuhaixv <49237108+Liuhaixv@users.noreply.github.com>
2022-06-25 19:27:12 +0800

    fixed hotkey glitch
    Fixed annoying glitch which causes ALT + TAB resetting all settings from ON to OFF

--- Le_Chiffre/misc/config.hpp
+++ Le_Chiffre/misc/config.hpp
@@ -6,7 +6,7 @@
 
 namespace {
 	// Config
-	const ::std::string CHEAT_VERSION = XorStr("v1.4.28");
+	const ::std::string CHEAT_VERSION = XorStr("v1.4.29");
 	const ::std::string RELEASE_DATE = XorStr("[25 June, 2022]");
 	const wchar_t* HOST = L"api.github.com";
 	const wchar_t* PATH = L"/repos/blaumaus/le_chiffre/tags?per_page=1";
